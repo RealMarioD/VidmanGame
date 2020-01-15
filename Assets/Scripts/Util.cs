65bdbb138df6664b4ea38feb234adb6d990ceada
Pannoniae <prisznyak.zsombor@gmail.com>
2020-01-15 22:21:07 +0000

    whatever

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -4,11 +4,13 @@
 using System;
 
 public static class Util {
+    private const double TOLERANCE = 0.001;
+
     public static bool EqualsFloat(this float n1, float n2) {
-        return Math.Abs(n1 - n2) < 0.001;
+        return Math.Abs(n1 - n2) < TOLERANCE;
     }
 
     public static bool EqualsZero(this float n1) {
-        return Math.Abs(n1) < 0.001;
+        return Math.Abs(n1) < TOLERANCE;
     }
 }
